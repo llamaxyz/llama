b61dd053e463bcc390e5ba2f023224b1f1b3fc79
dd0sxx <theo@llama.xyz>
2023-01-11 17:06:48 -0500

    delete Role

--- src/VertexERC721.sol
+++ src/VertexERC721.sol
@@ -10,18 +10,19 @@ struct Permission {
     address executor;
 }
 
-contract SentryERC721 is ERC721, Ownable {
+contract VertexERC721 is ERC721, Ownable {
     mapping(uint256 => string[]) public tokenToRoles;
     mapping(string => uint256[]) public rolesToPermissionSignatures;
+    string[] public roles;
     uint256 private _totalSupply;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
     function getPermissionSignatures(uint256 tokenId) public view returns (uint256[] memory) {
-        string[] storage roles = tokenToRoles[tokenId];
+        string[] memory userRoles = tokenToRoles[tokenId];
         uint256[] memory permissionSignatures;
-        for (uint256 i; i < roles.length; i++) {
-            uint256[] memory rolePermissionSignatures = rolesToPermissionSignatures[roles[i]];
+        for (uint256 i; i < userRoles.length; i++) {
+            uint256[] memory rolePermissionSignatures = rolesToPermissionSignatures[userRoles[i]];
             for (uint256 j; j < rolePermissionSignatures.length; j++) {
                 permissionSignatures[permissionSignatures.length - 1] = rolePermissionSignatures[j];
             }
@@ -47,13 +48,14 @@ contract SentryERC721 is ERC721, Ownable {
         return _totalSupply;
     }
 
-    function mint(address to, string[] calldata roles) public onlyOwner {
+    function mint(address to, string[] memory userRoles) public onlyOwner {
         uint256 tokenId = totalSupply() + 1;
         _mint(to, tokenId);
-        tokenToRoles[tokenId] = roles;
+        tokenToRoles[tokenId] = userRoles;
     }
 
     function addRole(string calldata role, Permission[] calldata permissions) public onlyOwner {
+        roles.push(role);
         for (uint256 i; i < permissions.length; i++) {
             uint256 permissionSignature = hashPermission(permissions[i]);
             rolesToPermissionSignatures[role][rolesToPermissionSignatures[role].length - 1] = permissionSignature;
@@ -62,5 +64,23 @@ contract SentryERC721 is ERC721, Ownable {
 
     function assignRole(uint256 tokenId, string calldata role) public onlyOwner {
         tokenToRoles[tokenId].push(role);
+    }
+
+    function revokeRole(uint256 tokenId, string calldata role) public onlyOwner {
+        string[] storage userRoles = tokenToRoles[tokenId];
+        for (uint256 i; i < userRoles.length; i++) {
+            if (keccak256(abi.encodePacked(roles[i])) == keccak256(abi.encodePacked(role))) {
+                delete roles[i];
+            }
+        }
+    }
+
+    function deleteRole(string calldata role) public onlyOwner {
+        delete rolesToPermissionSignatures[role];
+        for (uint256 i; i < roles.length; i++) {
+            if (keccak256(abi.encodePacked(roles[i])) == keccak256(abi.encodePacked(role))) {
+                delete roles[i];
+            }
+        }
     }
 }
