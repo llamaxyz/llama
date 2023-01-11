b0ff1be6310343b9e72fdccb1be800b4499c19d6
dd0sxx <theo@llama.xyz>
2023-01-11 11:04:26 -0500

    minimal permission struct

--- src/SentryERC721.sol
+++ src/SentryERC721.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/token/ERC721/ERC721.sol";
 
+struct Permission {
+    address target;
+    bytes4 signature;
+    address executor;
+}
+
 contract SentryERC721 is ERC721 {
+    mapping(string => Permission[]) permissions;
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 }
