7d30c6b0b08799493e09287298c8e42720882d6b
dd0sxx <theo@llama.xyz>
2023-01-11 13:36:15 -0500

    feat: added permissions data structures and views

--- src/SentryERC721.sol
+++ src/SentryERC721.sol
@@ -10,7 +10,34 @@ struct Permission {
 }
 
 contract SentryERC721 is ERC721 {
-    mapping(string => Permission[]) permissions;
+    mapping(uint256 => string[]) public tokenToRoles;
+    mapping(string => uint256[]) public rolesToPermissionSignatures;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function getPermissionSignatures(uint256 tokenId) public view returns (uint256[] memory) {
+        string[] storage roles = tokenToRoles[tokenId];
+        uint256[] memory permissionSignatures;
+        for (uint256 i; i < roles.length; i++) {
+            uint256[] memory rolePermissionSignatures = rolesToPermissionSignatures[roles[i]];
+            for (uint256 j; j < rolePermissionSignatures.length; j++) {
+                permissionSignatures[permissionSignatures.length - 1] = rolePermissionSignatures[j];
+            }
+        }
+        return permissionSignatures;
+    }
+
+    function hasPermission(uint256 tokenId, uint256 permissionSignature) public view returns (bool) {
+        uint256[] memory permissionSignatures = getPermissionSignatures(tokenId);
+        for (uint256 i; i < permissionSignatures.length; i++) {
+            if (permissionSignatures[i] == permissionSignature) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function hashPermission(Permission memory permission) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(permission.target, permission.signature, permission.executor)));
+    }
 }
