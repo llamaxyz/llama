628448ebcccef370ac67b0011881aa04208585c8
dd0sxx <theo@llama.xyz>
2023-01-11 09:57:22 -0500

    sentry nft boilerplate

--- src/SentryERC721.sol
+++ src/SentryERC721.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract SentryERC721 {}
+import "@openzeppelin/token/ERC721/ERC721.sol";
+
+contract SentryERC721 is ERC721 {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+}
