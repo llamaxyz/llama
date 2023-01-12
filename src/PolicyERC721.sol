6ad4ccffaf3b39a273f22894174ce4b02ef895af
dd0sxx <theo@llama.xyz>
2023-01-12 16:44:30 -0500

    note on deleted roles

--- src/PolicyERC721.sol
+++ src/PolicyERC721.sol
@@ -16,6 +16,9 @@ struct Permission {
 ///@title PolicyERC721
 ///@dev PolicyERC721 is a (TODO: soulbound) ERC721 contract where each token has roles and permissions
 ///@dev The roles and permissions determine how the token can interact with the vertex administrator contract
+///@note one behavior with this contract is that if a role is deleted, it will still appear in the tokenToRoles mapping.
+/// to solve this, we could run a pre check on all role based access functions that checks if the role exists in the
+/// roles array, and if not we delete that role from the tokenToRoles mapping, however, this adds gas to every call.
 
 contract PolicyERC721 is ERC721, Ownable {
     mapping(uint256 => string[]) public tokenToRoles;
