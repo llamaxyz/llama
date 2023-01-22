67dd0f0b687087dae568c415550dec621c89fd7b
Austin Green <austinzgreen@gmail.com>
2023-01-21 19:11:11 -0500

    action created test

--- tests/VertexCore.t.sol
+++ tests/VertexCore.t.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import {ProtocolVotingNFT} from "src/mock/ProtocolVotingNFT.sol";
 import {VertexCore} from "src/core/VertexCore.sol";
+import {ProtocolXYZ} from "src/mock/ProtocolXYZ.sol";
+import {VertexStrategy} from "src/strategy/VertexStrategy.sol";
 import {Strategy, WeightByPermission} from "src/utils/Structs.sol";
 
 contract VertexCoreTest is Test {
     VertexCore public vertex;
+    VertexStrategy public strategy;
+    ProtocolXYZ public protocol;
+    address public constant actionCreator = address(0x1337);
 
     function setUp() public {
         WeightByPermission[] memory approvalWeightByPermission = new WeightByPermission[](0);
@@ -25,10 +29,27 @@ contract VertexCoreTest is Test {
         });
 
         vertex = new VertexCore("ProtocolXYZ", "VXP", initialStrategies);
+        protocol = new ProtocolXYZ(address(vertex));
+
+        // Use create2 to get strategy's address
+        bytes32 strategySalt = bytes32(keccak256(abi.encode(initialStrategies[0])));
+        bytes memory bytecode = type(VertexStrategy).creationCode;
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(vertex),
+                strategySalt,
+                keccak256(abi.encodePacked(bytecode, abi.encode(initialStrategies[0], vertex.policy(), address(vertex))))
+            )
+        );
+        strategy = VertexStrategy(address(uint160(uint256(hash))));
+
+        vm.label(actionCreator, "Action Creator");
     }
 
-    function testPropose() public {
-        uint256 x = 1;
-        assertEq(x, 1);
+    function test_HappyActionFlow() public {
+        vm.startPrank(actionCreator);
+        vertex.createAction(strategy, address(protocol), 0, "function pause()", abi.encode(true));
+        assertEq(vertex.actionsCount(), 1);
     }
 }
