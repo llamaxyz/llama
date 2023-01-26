e390ab18a91bfc4177f72ffa9bb9156af8e160da
dd0sxx <theo@llama.xyz>
2023-01-26 15:11:54 -0700

    replaced empty brackets w ;

--- src/policy/VertexPolicy.sol
+++ src/policy/VertexPolicy.sol
@@ -15,39 +15,39 @@ abstract contract VertexPolicy is ERC721 {
     ///@notice mints multiple policy token with the given permissions
     ///@param to the addresses to mint the policy token to
     ///@param userPermissions the permissions to be granted to the policy token
-    function batchGrantPermissions(address[] memory to, bytes8[][] memory userPermissions) public virtual {}
+    function batchGrantPermissions(address[] memory to, bytes8[][] memory userPermissions) public virtual;
 
     ///@notice revokes all permissions from multiple policy tokens
     ///@param tokenIds the ids of the policy tokens to revoke permissions from
-    function batchRevokePermissions(uint256[] calldata tokenIds) public virtual {}
+    function batchRevokePermissions(uint256[] calldata tokenIds) public virtual;
 
     ///@notice Check if a holder has a permissionSignature at a specific block number
     ///@param policyholder the address of the policy holder
     ///@param permissionSignature the signature of the permission
     ///@param blockNumber the block number to query
-    function holderHasPermissionAt(address policyholder, bytes8 permissionSignature, uint256 blockNumber) external view virtual returns (bool) {}
+    function holderHasPermissionAt(address policyholder, bytes8 permissionSignature, uint256 blockNumber) external view virtual returns (bool);
 
     ///@notice sets the base URI for the contract
     ///@param _baseURI the base URI string to set
-    function setBaseURI(string memory _baseURI) public virtual {}
+    function setBaseURI(string memory _baseURI) public virtual;
 
     ///@notice Total number of policy NFTs at specific block number
     ///@param blockNumber the block number to query
-    function totalSupplyAt(uint256 blockNumber) external view virtual returns (uint256) {}
+    function totalSupplyAt(uint256 blockNumber) external view virtual returns (uint256);
 
     ///@notice Total number of policy NFTs at that have at least 1 of these permissions at specific block number
     ///@param permissions the permissions we are querying for
-    function getSupplyByPermissions(bytes8[] memory permissions) external view virtual returns (uint256) {}
+    function getSupplyByPermissions(bytes8[] memory permissions) external view virtual returns (uint256);
 
     ///@dev returns the total token supply of the contract
-    function totalSupply() public view virtual returns (uint256) {}
+    function totalSupply() public view virtual returns (uint256);
 
     ///@dev returns the permission signatures of a token
     ///@param tokenId the id of the token
-    function getPermissionSignatures(uint256 tokenId) public view virtual returns (bytes8[] memory) {}
+    function getPermissionSignatures(uint256 tokenId) public view virtual returns (bytes8[] memory);
 
     ///@dev checks if a token has a permission
     ///@param tokenId the id of the token
     ///@param permissionSignature the signature of the permission
-    function hasPermission(uint256 tokenId, bytes8 permissionSignature) public view virtual returns (bool) {}
+    function hasPermission(uint256 tokenId, bytes8 permissionSignature) public view virtual returns (bool);
 }
