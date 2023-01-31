575ff8627f192c4138b98d60b46387bc559ef46f
Rajath Alex <rajathalex@gmail.com>
2023-01-31 18:53:08 -0500

    initial Vertex Vault

---
+++ src/vault/VertexVault.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/// @title Vertex Vault
+/// @author Llama (vertex@llama.xyz)
+/// @notice The contract that holds the Vertex system's assets.
+contract VertexVault {}
