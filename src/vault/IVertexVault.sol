66ea15a02f2acd17a7cfdb2c8d4e9eb23365f07d
Rajath Alex <rajathalex@gmail.com>
2023-01-31 19:55:46 -0500

    IVertexVault

---
+++ src/vault/IVertexVault.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
+interface IVertexVault {
+    /// @notice Function for Vertex to give ERC20 allowance to other parties
+    /// @param token The address of the token to give allowance from
+    /// @param recipient Allowance's recipient
+    /// @param amount Allowance to approve
+    function approve(IERC20 token, address recipient, uint256 amount) external;
+
+    /// @notice Function for Vertex to transfer ERC20 tokens to other parties
+    /// @param token The address of the token to transfer
+    /// @param recipient Transfer's recipient
+    /// @param amount Amount to transfer
+    function transfer(IERC20 token, address recipient, uint256 amount) external;
+}
--- src/vault/VertexVault.sol
+++ src/vault/VertexVault.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {IVertexVault} from "src/vault/IVertexVault.sol";
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import {Address} from "@openzeppelin/utils/Address.sol";
@@ -8,7 +9,7 @@ import {Address} from "@openzeppelin/utils/Address.sol";
 /// @title Vertex Vault
 /// @author Llama (vertex@llama.xyz)
 /// @notice The contract that holds the Vertex system's assets.
-contract VertexVault {
+contract VertexVault is IVertexVault {
     using SafeERC20 for IERC20;
     using Address for address payable;
 
@@ -32,18 +33,12 @@ contract VertexVault {
     /// @notice Function for Vertex Vault to receive ETH
     receive() external payable {}
 
-    /// @notice Function for Vertex to give ERC20 allowance to other parties
-    /// @param token The address of the token to give allowance from
-    /// @param recipient Allowance's recipient
-    /// @param amount Allowance to approve
+    /// @inheritdoc IVertexVault
     function approve(IERC20 token, address recipient, uint256 amount) external onlyVertex {
         token.safeApprove(recipient, amount);
     }
 
-    /// @notice Function for Vertex to transfer ERC20 tokens to other parties
-    /// @param token The address of the token to transfer
-    /// @param recipient Transfer's recipient
-    /// @param amount Amount to transfer
+    /// @inheritdoc IVertexVault
     function transfer(IERC20 token, address recipient, uint256 amount) external onlyVertex {
         if (recipient == address(0)) revert Invalid0xRecipient();
 
