1d327805b75febc1ecd023e684da1ae5915b8599
Rajath Alex <rajathalex@gmail.com>
2023-02-17 15:57:25 -0500

    Making certain functions external

--- src/core/VertexCore.sol
+++ src/core/VertexCore.sol
@@ -221,12 +221,12 @@ contract VertexCore is IVertexCore, Initializable {
     }
 
     /// @inheritdoc IVertexCore
-    function createAndAuthorizeStrategies(Strategy[] calldata strategies) public override onlyVertex {
+    function createAndAuthorizeStrategies(Strategy[] calldata strategies) external override onlyVertex {
         _deployStrategies(strategies, policy);
     }
 
     /// @inheritdoc IVertexCore
-    function unauthorizeStrategies(VertexStrategy[] calldata strategies) public override onlyVertex {
+    function unauthorizeStrategies(VertexStrategy[] calldata strategies) external override onlyVertex {
         uint256 strategiesLength = strategies.length;
         unchecked {
             for (uint256 i = 0; i < strategiesLength; ++i) {
@@ -237,7 +237,7 @@ contract VertexCore is IVertexCore, Initializable {
     }
 
     /// @inheritdoc IVertexCore
-    function createAndAuthorizeAccounts(string[] calldata accounts) public override onlyVertex {
+    function createAndAuthorizeAccounts(string[] calldata accounts) external override onlyVertex {
         _deployAccounts(accounts);
     }
 
