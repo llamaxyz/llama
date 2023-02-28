a9fdf4aa46835fbbbf255baa7b9c05b1084928b2
Matt Solomon <matt@mattsolomon.dev>
2023-02-28 10:48:07 -0800

    test: adds invariant tests for protocol contracts (#100)
    **Motivation:**
    
    Sets up invariant tests and adds a few.
    
    **Modifications:**
    
    There's a lot, I'll let the code speak for itself. Let me know if I
    should add docs to the readme or something explaining how everything
    works
    
    **Result:**
    
    Run only invariant tests with `forge test --mp
    "tests/invariants/*.t.sol" -vvv`. Invariant tests are also run as part
    of all tests when `forge test` is ran.

--- tests/VertexFactory.t.sol
+++ tests/VertexFactory.t.sol
@@ -195,7 +195,7 @@ contract Deploy is VertexFactoryTest {
     // helper method, so if those parameters change, or we change the constructor signature, these
     // will need to be updated.
     address constant NEW_VERTEX = 0x5Fa39CD9DD20a3A77BA0CaD164bD5CF0d7bb3303;
-    address constant NEW_POLICY = 0x43dC4652D41415cC45B7036FaE8ea656fF72630a;
+    address constant NEW_POLICY = 0xfC3Fb7DeCfFEEe8AC37d488d37522164DB0eB57E;
 
     function deployVertex() internal returns (VertexCore) {
         Strategy[] memory initialStrategies = createInitialStrategies();
