f38eafb5269c12ea2c3fb0904327b0e4b66b98a2
Rajath Alex <rajathalex@gmail.com>
2023-03-15 18:09:00 -0400

    Removing IVertexStrategy

--- src/VertexStrategy.sol
+++ src/VertexStrategy.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import {Initializable} from "@openzeppelin/proxy/utils/Initializable.sol";
-import {IVertexStrategy} from "src/interfaces/IVertexStrategy.sol";
 import {VertexCore} from "src/VertexCore.sol";
 import {VertexPolicy} from "src/VertexPolicy.sol";
 import {Action, Strategy} from "src/lib/Structs.sol";
@@ -11,7 +10,14 @@ import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
 /// @title A strategy definition of a Vertex system.
 /// @author Llama (vertex@llama.xyz)
 /// @notice This is the template for Vertex strategies which determine the rules of an action's process.
-contract VertexStrategy is IVertexStrategy, Initializable {
+contract VertexStrategy is Initializable {
+  error InvalidPermissionId();
+  error NoPolicy();
+
+  event ForceApprovalRoleAdded(bytes32 role);
+  event ForceDisapprovalRoleAdded(bytes32 role);
+  event NewStrategyCreated(VertexCore vertex, VertexPolicy policy);
+
   /// @notice Equivalent to 100%, but in basis points.
   uint256 internal constant ONE_HUNDRED_IN_BPS = 10_000;
 
@@ -54,12 +60,12 @@ contract VertexStrategy is IVertexStrategy, Initializable {
 
   constructor() initializer {}
 
-  /// @notice Order is of WeightByPermissions is critical. Weight is determined by the first specific permission match.
-  function initialize(Strategy memory strategyConfig, VertexPolicy _policy, VertexCore _vertex)
-    external
-    override
-    initializer
-  {
+  /// @notice Initializes a new VertexStrategy clone.
+  /// @dev Order is of WeightByPermissions is critical. Weight is determined by the first specific permission match.
+  /// @param strategyConfig The strategy configuration.
+  /// @param _policy The policy contract.
+  /// @param _vertex The VertexCore contract.
+  function initialize(Strategy memory strategyConfig, VertexPolicy _policy, VertexCore _vertex) external initializer {
     queuingPeriod = strategyConfig.queuingPeriod;
     expirationPeriod = strategyConfig.expirationPeriod;
     isFixedLengthApprovalPeriod = strategyConfig.isFixedLengthApprovalPeriod;
@@ -87,25 +93,37 @@ contract VertexStrategy is IVertexStrategy, Initializable {
     emit NewStrategyCreated(_vertex, _policy);
   }
 
-  /// @inheritdoc IVertexStrategy
-  function isActionPassed(uint256 actionId) external view override returns (bool) {
+  /// @notice Get whether an action has passed the approval process.
+  /// @param actionId id of the action.
+  /// @return Boolean value that is true if the action has passed the approval process.
+  function isActionPassed(uint256 actionId) external view returns (bool) {
     Action memory action = vertex.getAction(actionId);
     return action.totalApprovals >= getMinimumAmountNeeded(action.approvalPolicySupply, minApprovalPct);
   }
 
-  /// @inheritdoc IVertexStrategy
-  function isActionCancelationValid(uint256 actionId) external view override returns (bool) {
+  /// @notice Get whether an action has eligible to be canceled.
+  /// @param actionId id of the action.
+  /// @return Boolean value that is true if the action can be canceled.
+  function isActionCancelationValid(uint256 actionId) external view returns (bool) {
     Action memory action = vertex.getAction(actionId);
     return action.totalDisapprovals >= getMinimumAmountNeeded(action.disapprovalPolicySupply, minDisapprovalPct);
   }
 
-  /// @inheritdoc IVertexStrategy
+  /// @notice Get the weight of an approval of a policyholder at a specific timestamp.
+  /// @param policyholder Address of the policyholder.
+  /// @param policyholder The role to check weight for.
+  /// @param timestamp The block number at which to get the approval weight.
+  /// @return The weight of the policyholder's approval.
   function getApprovalWeightAt(address policyholder, bytes32 role, uint256 timestamp) external view returns (uint256) {
     if (forceApprovalRole[role]) return type(uint256).max;
     return policy.holderWeightAt(policyholder, role, timestamp);
   }
 
-  /// @inheritdoc IVertexStrategy
+  /// @notice Get the weight of a disapproval of a policyholder at a specific timestamp.
+  /// @param policyholder Address of the policyholder.
+  /// @param policyholder The role to check weight for.
+  /// @param timestamp The block number at which to get the disapproval weight.
+  /// @return The weight of the policyholder's disapproval.
   function getDisapprovalWeightAt(address policyholder, bytes32 role, uint256 timestamp)
     external
     view
@@ -115,8 +133,11 @@ contract VertexStrategy is IVertexStrategy, Initializable {
     return policy.holderWeightAt(policyholder, role, timestamp);
   }
 
-  /// @inheritdoc IVertexStrategy
-  function getMinimumAmountNeeded(uint256 supply, uint256 minPct) public pure override returns (uint256) {
+  /// @notice Determine the minimum weight needed for an action to reach quorum.
+  /// @param supply Total number of policyholders eligible for participation.
+  /// @param minPct Minimum percentage needed to reach quorum.
+  /// @return The total weight needed to reach quorum.
+  function getMinimumAmountNeeded(uint256 supply, uint256 minPct) public pure returns (uint256) {
     // Rounding Up
     return FixedPointMathLib.mulDivUp(supply, minPct, ONE_HUNDRED_IN_BPS);
   }
