423e7f06f5bb66425f4f7450f43d9b923ac2357f
Matt Solomon <matt@mattsolomon.dev>
2023-03-20 09:32:03 -0700

    refactor: organize policy contract

--- src/VertexPolicy.sol
+++ src/VertexPolicy.sol
@@ -89,49 +89,9 @@ contract VertexPolicy is ERC721NonTransferableMinimalProxy {
     vertex = _vertex;
   }
 
-  /// @notice Returns the quantity of the `role` for the given `user`. The returned value is the
-  /// weight of the role when approving/disapproving (regardless of strategy).
-  /// @dev In the current implementation, this will always return 0 or 1 since quantities larger
-  /// than 1 are not supported. This is to keep the implementation simple
-  function getWeight(address user, bytes32 role) external view returns (uint256) {
-    uint256 tokenId = _tokenId(user);
-    (bool exists,, uint64 expiration, uint128 quantity) = roleBalanceCkpts[tokenId][role].latestCheckpoint();
-    return exists && quantity > 0 && expiration > block.timestamp ? quantity : 0;
-  }
-
-  /// @notice Returns the quantity of the `role` for the given `user` at `timestamp`. The returned
-  /// value is the weight of the role when approving/disapproving (regardless of strategy).
-  /// @dev In the current implementation, this will always return 0 or 1 since quantities larger
-  /// than 1 are not supported. This is to keep the implementation simple
-  function getPastWeight(address user, bytes32 role, uint256 timestamp) external view returns (uint256) {
-    uint256 tokenId = _tokenId(user);
-    (uint256 quantity, uint256 expiration) = roleBalanceCkpts[tokenId][role].getCheckpointAtTimestamp(timestamp);
-    return quantity > 0 && expiration > block.timestamp ? quantity : 0;
-  }
-
-  /// @notice Returns the total supply of `role` holders at the given `timestamp`. The returned
-  /// value is the value used to determine if quorum has been reached when approving/disapproving.
-  /// @dev The value returned by this method must equal the sum of the quantity of the role
-  /// across all policyholders at that timestamp.
-  function getSupply(bytes32 role) external view returns (uint256) {
-    (,,, uint128 quantity) = roleSupplyCkpts[role].latestCheckpoint();
-    return quantity;
-  }
-
-  /// @notice Returns the total supply of `role` holders at the given `timestamp`. The returned
-  /// value is the value used to determine if quorum has been reached when approving/disapproving.
-  /// @dev The value returned by this method must equal the sum of the quantity of the role
-  /// across all policyholders at that timestamp.
-  function getPastSupply(bytes32 role, uint256 timestamp) external view returns (uint256) {
-    return roleSupplyCkpts[role].getAtTimestamp(timestamp);
-  }
-
-  /// @notice Assigns roles to users.
-  function setRoleHolders(RoleHolderData[] memory roleHolders) external onlyVertex {
-    for (uint256 i = 0; i < roleHolders.length; i++) {
-      _setRoleHolder(roleHolders[i]);
-    }
-  }
+  // =======================================
+  // ======== Permission Management ========
+  // =======================================
 
   /// @notice Sets the permissions for a given role.
   function setRolePermissions(RolePermissionData[] memory rolePermissions) external onlyVertex {
@@ -170,6 +130,95 @@ contract VertexPolicy is ERC721NonTransferableMinimalProxy {
     _burn(_tokenId(user));
   }
 
+  /// @notice Assigns roles to users.
+  function setRoleHolders(RoleHolderData[] memory roleHolders) external onlyVertex {
+    for (uint256 i = 0; i < roleHolders.length; i++) {
+      _setRoleHolder(roleHolders[i]);
+    }
+  }
+
+  // =================================
+  // ======== ERC-721 Methods ========
+  // =================================
+
+  /// @notice sets the base URI for the contract
+  /// @param _baseURI the base URI string to set
+  function setBaseURI(string calldata _baseURI) public onlyVertex {
+    baseURI = _baseURI;
+  }
+
+  /// @dev overriding transferFrom to disable transfers
+  /// @dev this is a temporary solution, we will need to conform to a Soulbound standard
+  function transferFrom(address, /* from */ address, /* to */ uint256 /* policyId */ )
+    public
+    pure
+    override
+    nonTransferableToken
+  {}
+
+  /// @dev overriding safeTransferFrom to disable transfers
+  function safeTransferFrom(address, /* from */ address, /* to */ uint256 /* id */ )
+    public
+    pure
+    override
+    nonTransferableToken
+  {}
+
+  /// @dev overriding safeTransferFrom to disable transfers
+  function safeTransferFrom(address, /* from */ address, /* to */ uint256, /* policyId */ bytes calldata /* data */ )
+    public
+    pure
+    override
+    nonTransferableToken
+  {}
+
+  /// @dev overriding approve to disable approvals
+  function approve(address, /* spender */ uint256 /* id */ ) public pure override nonTransferableToken {}
+
+  /// @dev overriding approve to disable approvals
+  function setApprovalForAll(address, /* operator */ bool /* approved */ ) public pure override nonTransferableToken {}
+
+  // ====================================
+  // ======== Permission Getters ========
+  // ====================================
+
+  /// @notice Returns the quantity of the `role` for the given `user`. The returned value is the
+  /// weight of the role when approving/disapproving (regardless of strategy).
+  /// @dev In the current implementation, this will always return 0 or 1 since quantities larger
+  /// than 1 are not supported. This is to keep the implementation simple
+  function getWeight(address user, bytes32 role) external view returns (uint256) {
+    uint256 tokenId = _tokenId(user);
+    (bool exists,, uint64 expiration, uint128 quantity) = roleBalanceCkpts[tokenId][role].latestCheckpoint();
+    return exists && quantity > 0 && expiration > block.timestamp ? quantity : 0;
+  }
+
+  /// @notice Returns the quantity of the `role` for the given `user` at `timestamp`. The returned
+  /// value is the weight of the role when approving/disapproving (regardless of strategy).
+  /// @dev In the current implementation, this will always return 0 or 1 since quantities larger
+  /// than 1 are not supported. This is to keep the implementation simple
+  function getPastWeight(address user, bytes32 role, uint256 timestamp) external view returns (uint256) {
+    uint256 tokenId = _tokenId(user);
+    (uint256 quantity, uint256 expiration) = roleBalanceCkpts[tokenId][role].getCheckpointAtTimestamp(timestamp);
+    return quantity > 0 && expiration > block.timestamp ? quantity : 0;
+  }
+
+  /// @notice Returns the total supply of `role` holders at the given `timestamp`. The returned
+  /// value is the value used to determine if quorum has been reached when approving/disapproving.
+  /// @dev The value returned by this method must equal the sum of the quantity of the role
+  /// across all policyholders at that timestamp.
+  function getSupply(bytes32 role) external view returns (uint256) {
+    (,,, uint128 quantity) = roleSupplyCkpts[role].latestCheckpoint();
+    return quantity;
+  }
+
+  /// @notice Returns the total supply of `role` holders at the given `timestamp`. The returned
+  /// value is the value used to determine if quorum has been reached when approving/disapproving.
+  /// @dev The value returned by this method must equal the sum of the quantity of the role
+  /// across all policyholders at that timestamp.
+  function getPastSupply(bytes32 role, uint256 timestamp) external view returns (uint256) {
+    return roleSupplyCkpts[role].getAtTimestamp(timestamp);
+  }
+
   /// @notice Returns all checkpoints for the given `user` and `role`.
   function roleBalanceCheckpoints(address user, bytes32 role) external view returns (Checkpoints.History memory) {
     uint256 tokenId = _tokenId(user);
@@ -200,6 +249,58 @@ contract VertexPolicy is ERC721NonTransferableMinimalProxy {
     bool userHasRole = exists && quantity > 0 && expiration > block.timestamp;
     return userHasRole && canCreateAction[role][permissionId];
   }
+
+  /// @notice Returns the total number of policies in existence.
+  function totalSupply() public view returns (uint256) {
+    return roleSupplyCkpts[ALL_HOLDERS_ROLE].latest();
+  }
+
+  // =================================
+  // ======== ERC-721 Getters ========
+  // =================================
+
+  /// @notice returns the location of the policy metadata
+  /// @param tokenId the id of the policy token
+  function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    string[11] memory parts;
+
+    parts[0] =
+      '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" />';
+    parts[1] =
+      '<path transform="translate(10,10)" d="M3.0543 18.4836C3.05685 18.2679 3.14276 18.0616 3.29402 17.908C3.44526 17.7544 3.65009 17.6653 3.86553 17.6596H14.6671C15.177 17.6606 15.682 17.5611 16.1534 17.3667C16.6248 17.1724 17.0533 16.8869 17.4143 16.5267C17.7754 16.1665 18.062 15.7386 18.2577 15.2674C18.4534 14.7963 18.5545 14.2912 18.555 13.781V0H15.4987V13.781C15.4961 13.9967 15.4102 14.2029 15.2589 14.3566C15.1077 14.5102 14.9029 14.5993 14.6874 14.605H3.87567C2.84811 14.6061 1.86294 15.0151 1.13634 15.7422C0.409745 16.4694 0.00107373 17.4553 0 18.4836V27.9963H3.0543V18.4836Z" fill="url(#paint0_linear_2141_86430)"/><path transform="translate(10,10)" d="M19.9061 2.62599H19.7701L19.8999 2.7559V5.59734H22.7109L24.0292 6.92876C23.1533 7.10776 22.3661 7.58374 21.8004 8.27633C21.2346 8.96892 20.9252 9.83566 20.924 10.7302V28H23.9662V10.7261C23.9694 10.5086 24.0571 10.3008 24.2108 10.1469C24.3646 9.99309 24.5723 9.90526 24.7896 9.90211H25.2581C27.0529 9.90211 27.6615 8.90152 27.8419 8.4814C28.0224 8.06126 28.3003 6.91455 27.0308 5.63994L24.0534 2.63615H23.1265" fill="url(#paint1_linear_2141_86430)"/><path transform="translate(10,10)" d="M11.828 26.4971C12.4952 26.4965 13.1559 26.6289 13.7715 26.8864C14.3871 27.1439 14.9453 27.5214 15.4137 27.9969H19.1109C18.4455 26.6309 17.4099 25.4796 16.1222 24.6742C14.8345 23.8688 13.3465 23.4418 11.828 23.4418C10.3095 23.4418 8.82159 23.8688 7.53388 24.6742C6.2462 25.4796 5.21058 26.6309 4.54515 27.9969H8.24237C8.71076 27.5214 9.269 27.1439 9.88461 26.8864C10.5002 26.6289 11.1608 26.4965 11.828 26.4971Z" fill="url(#paint2_linear_2141_86430)"/><path transform="translate(10,10)" d="M36.616 11.494V11.824L41.17 23H44.756L49.288 11.824V11.494H45.878L43.062 19.678H42.842L40.026 11.494H36.616ZM49.53 17.236C49.53 21.152 52.214 23.264 55.712 23.264C59.166 23.264 60.816 21.196 61.3 19.59V19.26H58.286C58.066 19.986 57.428 21.02 55.712 21.02C53.908 21.02 52.874 19.656 52.83 18.006H61.498V17.06C61.498 13.32 59.078 11.23 55.624 11.23C52.214 11.23 49.53 13.32 49.53 17.236ZM52.852 16.048C52.962 14.618 53.908 13.474 55.668 13.474C57.406 13.474 58.33 14.618 58.396 16.048H52.852ZM63.6437 11.494V23H66.8777V17.324C66.8777 14.97 67.9337 13.914 70.0237 13.914H72.1357V11.45H70.1117C68.3077 11.45 67.4057 12.22 66.9657 13.254H66.7457V11.494H63.6437ZM73.3455 11.494V13.65H76.7115V19.854C76.7115 21.9 77.9875 23 80.0775 23H83.9055V20.734H80.1215L79.9455 20.558V13.65H83.6855V11.494H79.9455V7.6H79.6155L76.7115 8.964V11.494H73.3455ZM85.2181 17.236C85.2181 21.152 87.9021 23.264 91.4001 23.264C94.8541 23.264 96.5041 21.196 96.9881 19.59V19.26H93.9741C93.7541 19.986 93.1161 21.02 91.4001 21.02C89.5961 21.02 88.5621 19.656 88.5181 18.006H97.1861V17.06C97.1861 13.32 94.7661 11.23 91.3121 11.23C87.9021 11.23 85.2181 13.32 85.2181 17.236ZM88.5401 16.048C88.6501 14.618 89.5961 13.474 91.3561 13.474C93.0941 13.474 94.0181 14.618 94.0841 16.048H88.5401ZM98.0558 11.494V11.824L101.994 17.082L97.5498 22.67V23H101.048L103.952 19.172H104.172L106.944 23H110.354V22.67L106.306 17.302L110.64 11.824V11.494H107.186L104.348 15.212H104.128L101.466 11.494H98.0558Z" fill="white"/><defs><linearGradient id="paint0_linear_2141_86430" x1="15.9481" y1="2.22356e-07" x2="8.77168" y2="26.0208" gradientUnits="userSpaceOnUse"><stop stop-color="#0C97D4"/><stop offset="1" stop-color="#21CE99"/></linearGradient><linearGradient id="paint1_linear_2141_86430" x1="15.9481" y1="2.22356e-07" x2="8.77168" y2="26.0208" gradientUnits="userSpaceOnUse"><stop stop-color="#0C97D4"/><stop offset="1" stop-color="#21CE99"/></linearGradient><linearGradient id="paint2_linear_2141_86430" x1="15.9481" y1="2.22356e-07" x2="8.77168" y2="26.0208" gradientUnits="userSpaceOnUse"><stop stop-color="#0C97D4"/><stop offset="1" stop-color="#21CE99"/></linearGradient></defs>';
+    parts[2] = '<text x="10" y="60" class="base">';
+    parts[3] = string.concat("Policy Id: ", LibString.toString(tokenId));
+    parts[5] = '</text><text x="10" y="80" class="base">';
+    parts[6] = name;
+    parts[8] = '</text><text x="10" y="100" class="base">';
+    parts[9] = symbol;
+    parts[10] = "</text></svg>";
+
+    string memory output = string(
+      abi.encodePacked(
+        parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8], parts[9], parts[10]
+      )
+    );
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "Vertex Policy ID: ',
+            LibString.toString(tokenId),
+            '", "description": "Vertex is a identity access system for privledged smart contract functions", "image": "data:image/svg+xml;base64,',
+            Base64.encode(bytes(output)),
+            '"}'
+          )
+        )
+      )
+    );
+    return string(abi.encodePacked("data:application/json;base64,", json));
+  }
+
+  // ================================
+  // ======== Internal Logic ========
+  // ================================
 
   function _setRoleHolder(RoleHolderData memory roleHolder) internal {
     (bytes32 role, address user, uint256 expiration) = (roleHolder.role, roleHolder.user, roleHolder.expiration);
@@ -245,10 +346,6 @@ contract VertexPolicy is ERC721NonTransferableMinimalProxy {
     _setRoleHolder(RoleHolderData(expiredRole.role, expiredRole.user, 0));
   }
 
-  function _tokenId(address user) internal pure returns (uint256) {
-    return uint256(uint160(user));
-  }
-
   function _mint(address user) internal {
     _mint(user, _tokenId(user));
     roleSupplyCkpts[ALL_HOLDERS_ROLE].push(roleSupplyCkpts[ALL_HOLDERS_ROLE].latest() + 1);
@@ -259,67 +356,7 @@ contract VertexPolicy is ERC721NonTransferableMinimalProxy {
     roleSupplyCkpts[ALL_HOLDERS_ROLE].push(roleSupplyCkpts[ALL_HOLDERS_ROLE].latest() - 1);
   }
 
-  /// @notice sets the base URI for the contract
-  /// @param _baseURI the base URI string to set
-  function setBaseURI(string calldata _baseURI) public onlyVertex {
-    baseURI = _baseURI;
-  }
-
-  /// @dev overriding transferFrom to disable transfers
-  /// @dev this is a temporary solution, we will need to conform to a Souldbound standard
-  function transferFrom(address, /* from */ address, /* to */ uint256 /* policyId */ )
-    public
-    pure
-    override
-    nonTransferableToken
-  {}
-
-  /// @dev overriding safeTransferFrom to disable transfers
-  function safeTransferFrom(address, /* from */ address, /* to */ uint256 /* id */ )
-    public
-    pure
-    override
-    nonTransferableToken
-  {}
-
-  /// @dev overriding safeTransferFrom to disable transfers
-  function safeTransferFrom(address, /* from */ address, /* to */ uint256, /* policyId */ bytes calldata /* data */ )
-    public
-    pure
-    override
-    nonTransferableToken
-  {}
-
-  /// @dev overriding approve to disable approvals
-  function approve(address, /* spender */ uint256 /* id */ ) public pure override nonTransferableToken {}
-
-  /// @dev overriding approve to disable approvals
-  function setApprovalForAll(address, /* operator */ bool /* approved */ ) public pure override nonTransferableToken {}
-
-  /// @notice Returns the total number of policies in existence.
-  function totalSupply() public view returns (uint256) {
-    return roleSupplyCkpts[ALL_HOLDERS_ROLE].latest();
-  }
-
-  /// @notice returns the location of the policy metadata
-  /// @param tokenId the id of the policy token
-  function tokenURI(uint256 tokenId) public view override returns (string memory) {
-    return factory.tokenURI(name, symbol, tokenId);
-  }
-
-  function getTokenPermissionCheckpoints(uint256 policyId, bytes32 permissionId)
-    external
-    view
-    returns (PermissionIdCheckpoint[] memory)
-  {
-    return tokenPermissionCheckpoints[policyId][permissionId];
-  }
-
-  function getTokenPermissionSupplyCheckpoints(bytes32 permissionId)
-    external
-    view
-    returns (PermissionIdCheckpoint[] memory)
-  {
-    return permissionSupplyCheckpoints[permissionId];
+  function _tokenId(address user) internal pure returns (uint256) {
+    return uint256(uint160(user));
   }
 }
