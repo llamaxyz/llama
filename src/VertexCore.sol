f6eb4e36fdd4380a9084743525d325b74c23aa6c
Matt Solomon <matt@mattsolomon.dev>
2023-04-11 09:37:38 -0700

    chore: make typehashes internal

--- src/VertexCore.sol
+++ src/VertexCore.sol
@@ -75,20 +75,20 @@ contract VertexCore is Initializable {
   // =============================================================
 
   /// @notice EIP-712 base typehash.
-  bytes32 public constant EIP712_DOMAIN_TYPEHASH =
+  bytes32 internal constant EIP712_DOMAIN_TYPEHASH =
     keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
 
   /// @notice EIP-712 createAction typehash.
-  bytes32 public constant CREATE_ACTION_TYPEHASH = keccak256(
+  bytes32 internal constant CREATE_ACTION_TYPEHASH = keccak256(
     "CreateAction(uint8 role,address strategy,address target,uint256 value,bytes4 selector,bytes data,address policyholder,uint256 nonce)"
   );
 
   /// @notice EIP-712 castApproval typehash.
-  bytes32 public constant CAST_APPROVAL_TYPEHASH =
+  bytes32 internal constant CAST_APPROVAL_TYPEHASH =
     keccak256("CastApproval(uint256 actionId,uint8 role,string reason,address policyholder,uint256 nonce)");
 
   /// @notice EIP-712 castDisapproval typehash.
-  bytes32 public constant CAST_DISAPPROVAL_TYPEHASH =
+  bytes32 internal constant CAST_DISAPPROVAL_TYPEHASH =
     keccak256("CastDisapproval(uint256 actionId,uint8 role,string reason,address policyholder,uint256 nonce)");
 
   /// @notice Equivalent to 100%, but scaled for precision
