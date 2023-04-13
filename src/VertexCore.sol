bbdb35e3f34da24edf60c7d894755d9e2ce8bbe7
Matt Solomon <matt@mattsolomon.dev>
2023-04-12 21:31:44 -0700

    refactor: move logic into strategies, and fix weight bug (#230)
    Closes https://github.com/llama-community/vertex-v1/issues/220, closes
    https://github.com/llama-community/vertex-v1/issues/227
    
    **Motivation:**
    
    In #227 we had a bug when fetching weight which needs to be fixed
    
    In #220 we discussed moving logic out of core and into strategies to
    make things more flexible, as strategies can be changing with new logic
    added, but the core cannot be changed
    
    **Modifications:**
    
    - Move cancelation logic, expiration logic, and disapproval logic into
    strategy
    - Deduplicate a few custom errors that were similar to simplify the ABI
    - Also cleaned up a few tests to remove some unnecessary code + fix an
    invariant test bug
    - Rename the action `executionTime` field to `minExecutionTime`
    
    **Result:**
    
    The logic for (1) determining if an action can be canceled, (2)
    determining if an action is expired, and (3) determining if disapprovals
    are supported are all handled by the strategy, therefore they can be
    changed.
    
    Relative (percentage) vs. absolute voting thresholds can also be
    supported now. For example, relative voting would be supported by
    changing the strategy's `isActionPassed` from:
    
    ```solidity
    function isActionPassed(uint256 actionId) external view returns (bool) {
      Action memory action = vertex.getAction(actionId);
      return action.totalApprovals >= getMinimumAmountNeeded(action.approvalPolicySupply, minApprovalPct);
    }
    ```
    
    to 
    
    ```solidity
    function isActionPassed(uint256 actionId) external view returns (bool) {
      Action memory action = vertex.getAction(actionId);
      return action.totalApprovals >= ACTION_THRESHOLD;
    }
    ```
    
    where `ACTION_THRESHOLD` is a strategy constant. Now the obvious
    question is how does this constant value get set? It must be passed to
    the `initialize` method, but with the current interface that wouldn't be
    possible. Therefore my suggestion is to, in a separate PR:
    1. Create an ` interface IVertexStrategy` contract. This is the
    interface that strategies MUST conform to, and contains the set of
    functions that `VertexCore` (or other contracts in the protocol) will
    call on the strategy.
    2. Update types from `VertexStrategy` to `IVertexStrategy`
    3. Rename the current `VertexStrategy` contract to something more
    descriptive, like `VertexStrategyPercentageThresholds`
    4. The last thing is that we'll need to change the `initialize` method
    and standardize this in the interface—different strategies will have
    different parameters, so we'll need to change some parts to a generic
    `bytes` argument that the strategy itself decodes.
    
    This combination of changes will make strategies very flexible, with the
    only limitations being that they conform to the interface, and
    everything else can be changed.
    
    ## TWO ASKS:
    1. Let me know if there's anything else I missed that should be moved
    into the strategy contract
    2. Let me know if that interface refactor is what I should work on next

--- src/VertexCore.sol
+++ src/VertexCore.sol
@@ -24,12 +24,8 @@ contract VertexCore is Initializable {
   error InvalidPolicyholder();
   error InvalidCancelation();
   error InvalidActionId();
-  error OnlyQueuedActions();
-  error InvalidStateForQueue();
-  error ActionCannotBeCanceled();
+  error InvalidActionState(ActionState expected);
   error OnlyVertex();
-  error ActionNotActive();
-  error ActionNotQueued();
   error InvalidSignature();
   error TimelockNotFinished();
   error FailedActionExecution();
@@ -157,7 +153,7 @@ contract VertexCore is Initializable {
     name = _name;
     policy = _policy;
 
-    _deployStrategies(_vertexStrategyLogic, initialStrategies, _policy);
+    _deployStrategies(_vertexStrategyLogic, initialStrategies);
     _deployAccounts(_vertexAccountLogic, initialAccounts);
   }
 
@@ -241,11 +237,11 @@ contract VertexCore is Initializable {
   /// @notice Queue an action by actionId if it's in Approved state.
   /// @param actionId Id of the action to queue.
   function queueAction(uint256 actionId) external {
-    if (getActionState(actionId) != ActionState.Approved) revert InvalidStateForQueue();
+    if (getActionState(actionId) != ActionState.Approved) revert InvalidActionState(ActionState.Approved);
     Action storage action = actions[actionId];
     uint256 executionTime = block.timestamp + action.strategy.queuingPeriod();
 
-    action.executionTime = executionTime;
+    action.minExecutionTime = executionTime;
 
     emit ActionQueued(actionId, msg.sender, action.strategy, action.creator, executionTime);
   }
@@ -255,10 +251,10 @@ contract VertexCore is Initializable {
   /// @return The result returned from the call to the target contract.
   function executeAction(uint256 actionId) external payable returns (bytes memory) {
     // Initial checks that action is ready to execute.
-    if (getActionState(actionId) != ActionState.Queued) revert OnlyQueuedActions();
+    if (getActionState(actionId) != ActionState.Queued) revert InvalidActionState(ActionState.Queued);
 
     Action storage action = actions[actionId];
-    if (block.timestamp < action.executionTime) revert TimelockNotFinished();
+    if (block.timestamp < action.minExecutionTime) revert TimelockNotFinished();
     if (msg.value < action.value) revert InsufficientMsgValue();
 
     // Check pre-execution action guard.
@@ -285,22 +281,15 @@ contract VertexCore is Initializable {
     return result;
   }
 
-  /// @notice Cancels an action. Can be called anytime by the creator or if action is disapproved.
+  /// @notice Cancels an action. Rules for cancelation are defined by the strategy.
   /// @param actionId Id of the action to cancel.
   function cancelAction(uint256 actionId) external {
-    ActionState state = getActionState(actionId);
-    if (
-      state == ActionState.Executed || state == ActionState.Canceled || state == ActionState.Expired
-        || state == ActionState.Failed
-    ) revert InvalidCancelation();
-
+    // We don't need an explicit check on action existence because if it doesn't exist the strategy will be the zero
+    // address, and Solidity will revert when the `isActionCancelationValid` call has no return data.
     Action storage action = actions[actionId];
-    if (!(msg.sender == action.creator || action.strategy.isActionCancelationValid(actionId))) {
-      revert ActionCannotBeCanceled();
-    }
+    if (!action.strategy.isActionCancelationValid(actionId, msg.sender)) revert InvalidCancelation();
 
     action.canceled = true;
-
     emit ActionCanceled(actionId);
   }
 
@@ -413,7 +402,7 @@ contract VertexCore is Initializable {
     external
     onlyVertex
   {
-    _deployStrategies(vertexStrategyLogic, strategies, policy);
+    _deployStrategies(vertexStrategyLogic, strategies);
   }
 
   /// @notice Remove strategies from the mapping of authorized strategies.
@@ -442,14 +431,6 @@ contract VertexCore is Initializable {
     emit ActionGuardSet(target, selector, guard);
   }
 
-  /// @notice Get whether an action has expired and can no longer be executed.
-  /// @param actionId id of the action.
-  /// @return Boolean value that is true if the action has expired.
-  function isActionExpired(uint256 actionId) public view returns (bool) {
-    Action storage action = actions[actionId];
-    return block.timestamp >= action.executionTime + action.strategy.expirationPeriod();
-  }
-
   /// @notice Get an Action struct by actionId.
   /// @param actionId id of the action.
   /// @return The Action struct.
@@ -474,11 +455,11 @@ contract VertexCore is Initializable {
 
     if (!action.strategy.isActionPassed(actionId)) return ActionState.Failed;
 
-    if (action.executionTime == 0) return ActionState.Approved;
+    if (action.minExecutionTime == 0) return ActionState.Approved;
 
     if (action.executed) return ActionState.Executed;
 
-    if (isActionExpired(actionId)) return ActionState.Expired;
+    if (action.strategy.isActionExpired(actionId)) return ActionState.Expired;
 
     return ActionState.Queued;
   }
@@ -543,7 +524,7 @@ contract VertexCore is Initializable {
   }
 
   function _castApproval(address policyholder, uint8 role, uint256 actionId, string memory reason) internal {
-    if (getActionState(actionId) != ActionState.Active) revert ActionNotActive();
+    if (getActionState(actionId) != ActionState.Active) revert InvalidActionState(ActionState.Active);
     bool hasApproved = approvals[actionId][policyholder];
     if (hasApproved) revert DuplicateApproval();
 
@@ -562,7 +543,7 @@ contract VertexCore is Initializable {
   }
 
   function _castDisapproval(address policyholder, uint8 role, uint256 actionId, string memory reason) internal {
-    if (getActionState(actionId) != ActionState.Queued) revert ActionNotQueued();
+    if (getActionState(actionId) != ActionState.Queued) revert InvalidActionState(ActionState.Queued);
     bool hasDisapproved = disapprovals[actionId][policyholder];
     if (hasDisapproved) revert DuplicateDisapproval();
 
@@ -570,7 +551,7 @@ contract VertexCore is Initializable {
     bool hasRole = policy.hasRole(policyholder, role, action.creationTime);
     if (!hasRole) revert InvalidPolicyholder();
 
-    if (action.strategy.minDisapprovalPct() > ONE_HUNDRED_IN_BPS) revert DisapproveDisabled();
+    if (!action.strategy.isDisapprovalEnabled()) revert DisapproveDisabled();
 
     uint256 weight = action.strategy.getDisapprovalWeightAt(policyholder, role, action.creationTime);
 
@@ -582,9 +563,7 @@ contract VertexCore is Initializable {
     emit DisapprovalCast(actionId, policyholder, weight, reason);
   }
 
-  function _deployStrategies(address vertexStrategyLogic, Strategy[] calldata strategies, VertexPolicy _policy)
-    internal
-  {
+  function _deployStrategies(address vertexStrategyLogic, Strategy[] calldata strategies) internal {
     if (address(factory).code.length > 0 && !factory.authorizedStrategyLogics(vertexStrategyLogic)) {
       // The only edge case where this check is skipped is if `_deployStrategies()` is called by Root Vertex Instance
       // during Vertex Factory construction. This is because there is no code at the Vertex Factory address yet.
@@ -608,7 +587,7 @@ contract VertexCore is Initializable {
         );
 
         VertexStrategy strategy = VertexStrategy(Clones.cloneDeterministic(vertexStrategyLogic, salt));
-        strategy.initialize(strategies[i], _policy);
+        strategy.initialize(strategies[i]);
         authorizedStrategies[strategy] = true;
         emit StrategyAuthorized(strategy, vertexStrategyLogic, strategies[i]);
       }
