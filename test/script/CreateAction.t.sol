9b3a1500bbd0a75c437838136be5dcc68ff1fb92
David Laprade <david.laprade@gmail.com>
2023-04-18 16:36:17 -0400

    More tests

--- test/script/CreateAction.t.sol
+++ test/script/CreateAction.t.sol
@@ -6,6 +6,7 @@ import {Test, console2} from "forge-std/Test.sol";
 import {VertexCore} from "src/VertexCore.sol";
 import {VertexFactory} from "src/VertexFactory.sol";
 import {Action} from "src/lib/Structs.sol";
+import {ActionState} from "src/lib/Enums.sol";
 import {DeployVertex} from "script/DeployVertex.s.sol";
 import {CreateActionToDeployVertexInstance} from "script/CreateAction.s.sol";
 
@@ -27,6 +28,8 @@ contract Run is CreateActionTest {
   // automate action creation to deploy new Vertex instances. It could be
   // replaced with any address that we hold the private key for.
   address VERTEX_INSTANCE_DEPLOYER = 0x3d9fEa8AeD0249990133132Bb4BC8d07C6a8259a;
+
+  uint8 ACTION_CREATOR_ROLE_ID = 1;
 
   function test_deploy() public {
     // TODO revert if root vertex is not at the address in the deploy script
@@ -50,9 +53,45 @@ contract Run is CreateActionTest {
     assertFalse(action.canceled);
     assertEq(action.selector, VertexFactory.deploy.selector);
     assertEq(action.target, address(factory));
+    assertEq(
+      uint8(rootVertex.getActionState(newActionId)),
+      uint8(ActionState.Active)
+    );
   }
 
   function test_actionCanBeExecuted() public {
+    script.run(VERTEX_INSTANCE_DEPLOYER);
+
+    // Advance the clock so that checkpoints take effect.
+    vm.roll(block.number + 1);
+    vm.warp(block.timestamp + 1);
+
+    uint256 deployActionId = 0;
+
+    assertEq(
+      uint8(rootVertex.getActionState(deployActionId)),
+      uint8(ActionState.Active)
+    );
+
+    vm.prank(VERTEX_INSTANCE_DEPLOYER); // This EOA has force-approval permissions.
+    rootVertex.castApproval(deployActionId, ACTION_CREATOR_ROLE_ID);
+
+    assertEq(
+      uint8(rootVertex.getActionState(deployActionId)),
+      uint8(ActionState.Approved)
+    );
+
+    rootVertex.queueAction(deployActionId);
+
+    Action memory action = rootVertex.getAction(deployActionId);
+
+    // Advance the clock to execute the action.
+    vm.roll(block.number + 1);
+    vm.warp(action.minExecutionTime + 1);
+
+    bytes memory deployResult = rootVertex.executeAction(deployActionId);
+    VertexCore vertexInstance = abi.decode(deployResult, (VertexCore));
+    assertEq(address(vertexInstance.factory()), address(factory));
     // TODO confirm that a new vertex instance is created
     // TODO confirm new vertex instance properties
   }
