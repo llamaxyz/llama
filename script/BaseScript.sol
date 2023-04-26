a52bf1742c74cdd040a664fcd9d4fe370d93a48e
dd0sxx <theo@llama.xyz>
2023-04-26 12:36:25 -0600

    baseScript.s.sol

---
+++ script/BaseScript.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @dev A mock script that can be configured for testing.
+contract BaseScript /* is LlamaStorage */ {
+  function executeScript() external {
+  }
+  
+}
