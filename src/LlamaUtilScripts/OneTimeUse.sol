90f28e11073a2f097700350e4c706639f622b326
dd0sxx <theo@llama.xyz>
2023-04-27 11:53:40 -0600

    SELF

--- src/LlamaUtilScripts/OneTimeUse.sol
+++ src/LlamaUtilScripts/OneTimeUse.sol
@@ -4,18 +4,20 @@ pragma solidity ^0.8.19;
 import {LlamaCore} from "src/LlamaCore.sol";
 
 /// @dev A mock script that can be configured for testing.
+/// @notice this script is meant to be delegate called by the core contract, which informs our use of SELF and address(this)
+/// @notice this script is a template for creating new scripts, and should not be used directly
 contract OneTimeUse {
-  address private immutable THIS_ADDRESS;
+  address private immutable SELF;
   LlamaCore core;
 
   constructor() {
-    THIS_ADDRESS = address(this);
+    SELF = address(this);
   }
 
   modifier unauthorizeAfterRun() {
     _;
     core = LlamaCore(address(this));
-    core.authorizeScript(THIS_ADDRESS, false);
+    core.authorizeScript(SELF, false);
   }
 
   function run() external unauthorizeAfterRun {
