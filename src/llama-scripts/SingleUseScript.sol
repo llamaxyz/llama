20ecf83225c0d57114d1ccbd0eb8f593fced483d
dd0sxx <theo@llama.xyz>
2023-04-28 14:17:07 -0600

    SingleUseScript

--- src/llama-scripts/SingleUseScript.sol
+++ src/llama-scripts/SingleUseScript.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.19;
 
 import {LlamaCore} from "src/LlamaCore.sol";
 
-/// @notice This script is a template for creating new scripts, and should not be used directly
+/// @notice This script is a template for creating new scripts, and should not be used directly.
 /// @dev A mock script that can be configured for testing.
-/// @dev this script is meant to be delegate called by the core contract, which informs our use of `SELF` and
-/// `address(this)`
-contract OneTimeUse {
+/// @dev This script is meant to be delegate called by the core contract, which informs our use of `SELF` and
+/// `address(this)`.
+abstract contract SingleUseScript {
   address private immutable SELF;
   LlamaCore core;
 
@@ -15,13 +15,10 @@ contract OneTimeUse {
     SELF = address(this);
   }
 
+  /// @dev Add this to your script's methods and to unauthorize the script after it has been run once.
   modifier unauthorizeAfterRun() {
     _;
     core = LlamaCore(address(this));
     core.authorizeScript(SELF, false);
-  }
-
-  function run() external unauthorizeAfterRun {
-    // do something
   }
 }
