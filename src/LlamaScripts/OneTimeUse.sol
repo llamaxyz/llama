877f004da3a30b94889d5907de8c554ce190e0ec
dd0sxx <theo@llama.xyz>
2023-04-27 20:33:23 -0600

    reordered natspec

--- src/LlamaScripts/OneTimeUse.sol
+++ src/LlamaScripts/OneTimeUse.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.19;
 
 import {LlamaCore} from "src/LlamaCore.sol";
 
+/// @notice This script is a template for creating new scripts, and should not be used directly
 /// @dev A mock script that can be configured for testing.
-/// @notice this script is meant to be delegate called by the core contract, which informs our use of SELF and
-/// address(this)
-/// @notice this script is a template for creating new scripts, and should not be used directly
+/// @dev this script is meant to be delegate called by the core contract, which informs our use of `SELF` and `address(this)`
 contract OneTimeUse {
   address private immutable SELF;
   LlamaCore core;
