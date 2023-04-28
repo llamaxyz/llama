3015dc1950daf3c608d5efaea9286d71d0b9f57a
Matt Solomon <matt@mattsolomon.dev>
2023-04-28 09:49:11 -0700

    feat: broader bootstrap check

--- script/DeployUtils.sol
+++ script/DeployUtils.sol
@@ -153,6 +153,8 @@ library DeployUtils {
   }
 
   function bootstrapSafetyCheck(string memory filename) internal view {
+    // NOTE: This only supports relative strategies for now.
+
     // -------- Read data --------
     // Read the raw, encoded input file
     string memory jsonInput = readScriptInput(filename);
@@ -168,23 +170,50 @@ library DeployUtils {
     RelativeStrategyJsonInputs memory bootstrapStrategy = relativeStrategyConfigs[0];
 
     // -------- Validate data --------
-    // Ensure the bootstrap strategy uses the bootstrap role.
-    require(bootstrapStrategy.approvalRole == BOOTSTRAP_ROLE, "DeployLlama: bootstrap strategy uses wrong role");
+    // For a bootstrap strategy to passable, we need at least one of the following to be true:
+    //   1. The approval role is be the bootstrap role AND there are enough bootstrap role holders
+    //      to pass an action.
+    //   2. A force approval role is the bootstrap role AND there is at least one bootstrap role
+    //      holder.
 
     // Get the number of role holders with Role ID 1, which is the bootstrap role.
     uint256 bootstrapRoleSupply = 0;
     for (uint256 i = 0; i < roleHolderData.length; i++) {
       if (roleHolderData[i].role == BOOTSTRAP_ROLE) bootstrapRoleSupply++;
     }
-    require(bootstrapRoleSupply > 0, "DeployLlama: no bootstrap role holders");
 
-    // Based on the bootstrap strategy config and number of bootstrap role holders, compute the
-    // minimum number of role holders to pass a vote. The calculation here MUST match the one
-    // in the RelativeStrategy's `_getMinimumAmountNeeded` method. This check should never fail
-    // for relative strategies, but it's left in as a reminder that it needs to be checked for
-    // absolute strategies.
-    uint256 minPct = bootstrapStrategy.minApprovalPct;
-    uint256 numApprovalsRequired = FixedPointMathLib.mulDivUp(bootstrapRoleSupply, minPct, ONE_HUNDRED_IN_BPS);
-    require(bootstrapRoleSupply >= numApprovalsRequired, "DeployLlama: invalid bootstrap configuration");
+    // If no one holds that role, then the bootstrap strategy is not passable.
+    require(bootstrapRoleSupply > 0, "No one holds the bootstrap role");
+
+    // Check 1.
+    bool isCheck1Satisfied = false;
+    if (bootstrapStrategy.approvalRole == BOOTSTRAP_ROLE) {
+      // Based on the bootstrap strategy config and number of bootstrap role holders, compute the
+      // minimum number of role holders to pass a vote. The calculation here MUST match the one
+      // in the RelativeStrategy's `_getMinimumAmountNeeded` method. This check should never fail
+      // for relative strategies, but it's left in as a reminder that it needs to be checked for
+      // absolute strategies.
+      uint256 minPct = bootstrapStrategy.minApprovalPct;
+      uint256 numApprovalsRequired = FixedPointMathLib.mulDivUp(bootstrapRoleSupply, minPct, ONE_HUNDRED_IN_BPS);
+
+      if (bootstrapRoleSupply >= numApprovalsRequired) isCheck1Satisfied = true;
+    }
+
+    // Check 2.
+    bool isCheck2Satisfied = false;
+    for (uint256 i = 0; i < bootstrapStrategy.forceApprovalRoles.length; i++) {
+      if (bootstrapStrategy.forceApprovalRoles[i] == BOOTSTRAP_ROLE) {
+        isCheck2Satisfied = true;
+        break;
+      }
+    }
+
+    // If neither check is satisfied, the bootstrap strategy is invalid.
+    string memory check1Result = string.concat("\n  check1: ", isCheck1Satisfied ? "true" : "false");
+    string memory check2Result = string.concat("\n  check2: ", isCheck2Satisfied ? "true" : "false");
+    require(
+      isCheck1Satisfied || isCheck2Satisfied,
+      string.concat("Bootstrap strategy is invalid", check1Result, check2Result, "\n")
+    );
   }
 }
