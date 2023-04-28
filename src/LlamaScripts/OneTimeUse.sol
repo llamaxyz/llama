7c6b7bce066607dedd61c859b20b255b3189e6b3
dd0sxx <theo@llama.xyz>
2023-04-27 20:34:45 -0600

    fmt

--- src/LlamaScripts/OneTimeUse.sol
+++ src/LlamaScripts/OneTimeUse.sol
@@ -5,7 +5,8 @@ import {LlamaCore} from "src/LlamaCore.sol";
 
 /// @notice This script is a template for creating new scripts, and should not be used directly
 /// @dev A mock script that can be configured for testing.
-/// @dev this script is meant to be delegate called by the core contract, which informs our use of `SELF` and `address(this)`
+/// @dev this script is meant to be delegate called by the core contract, which informs our use of `SELF` and
+/// `address(this)`
 contract OneTimeUse {
   address private immutable SELF;
   LlamaCore core;
