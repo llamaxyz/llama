fb1e0987c06380ef0bb47497fffcdf51ad1dc6a0
dd0sxx <theo@llama.xyz>
2023-05-01 15:51:19 -0600

    removed console2 import

--- src/strategies/RelativeStrategy.sol
+++ src/strategies/RelativeStrategy.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {console2} from "forge-std/console2.sol";
 import {Initializable} from "@openzeppelin/proxy/utils/Initializable.sol";
 
 import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
