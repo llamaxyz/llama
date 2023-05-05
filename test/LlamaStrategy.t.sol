791324106d54f1e720da22c2996f9c9d4ad3b6ef
dd0sxx <theo@llama.xyz>
2023-05-05 17:38:12 -0600

    updated comment

--- test/LlamaStrategy.t.sol
+++ test/LlamaStrategy.t.sol
@@ -901,9 +901,8 @@ contract GetApprovalQuantityAt is LlamaStrategyTest {
     );
   }
 
-  function testFuzz_ReturnsZeroForNonApprovalRoles(uint8 _role, address _policyHolder, uint128 _quantity) public {
-    _role = uint8(bound(_role, 1, 8)); // ignoring all roles in the test setup to avoid conflicts with pre-assigned
-      // roles
+  function testFuzz_returnsZeroForNonApprovalRoles(uint8 _role, address _policyHolder, uint128 _quantity) public {
+    _role = uint8(bound(_role, 1, 8)); // only using roles in the test setup to avoid having to create new roles
     vm.assume(_role != uint8(Roles.TestRole1));
     vm.assume(_policyHolder != address(0));
     vm.assume(mpPolicy.balanceOf(_policyHolder) == 0);
