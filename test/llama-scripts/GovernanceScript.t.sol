d6f5271a0d47b45458e6d1dfb883c0c6a943db0c
Austin Green <austinzgreen@gmail.com>
2023-05-16 14:45:52 -0400

    typo

--- test/llama-scripts/GovernanceScript.t.sol
+++ test/llama-scripts/GovernanceScript.t.sol
@@ -277,7 +277,8 @@ contract SetRoleHolders is GovernanceScriptTest {
   function testFuzz_setRoleHolders(GovernanceScript.SetRoleHolder[] memory roleHolders) public {
     vm.assume(roleHolders.length < 500);
     for (uint256 i = 0; i < roleHolders.length; i++) {
-      roleHolders[i].role = uint8(bound(roleHolders[i].role, 1, mpPolicy.numRoles())); // number of exisitng roles (8) and cannot be 0
+      roleHolders[i].role = uint8(bound(roleHolders[i].role, 1, mpPolicy.numRoles())); // number of exisitng roles (8)
+        // and cannot be 0
         // (all holders role)
       vm.assume(roleHolders[i].expiration > block.timestamp + 1 days);
       vm.assume(roleHolders[i].policyholder != address(0));
@@ -306,7 +307,8 @@ contract SetRoleHolders is GovernanceScriptTest {
 contract SetRolePermissions is GovernanceScriptTest {
   function testFuzz_setRolePermissions(GovernanceScript.SetRolePermission[] memory rolePermissions) public {
     for (uint256 i = 0; i < rolePermissions.length; i++) {
-      rolePermissions[i].role = uint8(bound(rolePermissions[i].role, 1, mpPolicy.numRoles())); // number of exisitng roles (8) and cannot
+      rolePermissions[i].role = uint8(bound(rolePermissions[i].role, 1, mpPolicy.numRoles())); // number of existing
+        // roles (8) and cannot
         // be 0 (all holders role)
     }
     bytes memory data = abi.encodeWithSelector(SET_ROLE_PERMISSIONS_SELECTOR, rolePermissions);
