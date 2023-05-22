fef80a2953c80ce47290a2901b19933f3965bd27
dd0sxx <theo@llama.xyz>
2023-05-22 13:13:53 -0600

    base and single use script

--- src/llama-scripts/SingleUseScript.sol
+++ src/llama-scripts/SingleUseScript.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {BaseScript} from "src/llama-scripts/BaseScript.sol";
 import {LlamaCore} from "src/LlamaCore.sol";
+import {LlamaExecutor} from "src/LlamaExecutor.sol";
 
 /// @notice This script is a template for creating new scripts, and should not be used directly.
-/// @dev A mock script that can be configured for testing.
 /// @dev This script is meant to be delegate called by the core contract, which informs our use of `SELF` and
 /// `address(this)`.
-abstract contract SingleUseScript {
-  address private immutable SELF;
-  LlamaCore core;
+abstract contract SingleUseScript is BaseScript {
+  LlamaExecutor immutable EXECUTOR;
 
-  constructor() {
-    SELF = address(this);
+  constructor(address executor) {
+    EXECUTOR = LlamaExecutor(executor);
   }
 
   /// @dev Add this to your script's methods to unauthorize the script after it has been run once.
   modifier unauthorizeAfterRun() {
     _;
-    core = LlamaCore(address(this));
+    LlamaCore core = LlamaCore(EXECUTOR.LLAMA_CORE());
     core.authorizeScript(SELF, false);
   }
 }
