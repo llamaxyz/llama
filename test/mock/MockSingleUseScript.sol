4615a8b1eeb1362c1982d15ff0ca487923d0bc61
dd0sxx <contacttheotelonis@gmail.com>
2023-05-25 10:03:37 -0600

    Update test/mock/MockSingleUseScript.sol
    Co-authored-by: Rajath Alex <rajathalex@gmail.com>

--- test/mock/MockSingleUseScript.sol
+++ test/mock/MockSingleUseScript.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {SingleUseScript} from "src/llama-scripts/SingleUseScript.sol";
 import {LlamaExecutor} from "src/LlamaExecutor.sol";
 
-/// @dev This is a mock contract that inherits from the single us script for testing purposes
+/// @dev This is a mock contract that inherits from the single use script for testing purposes
 contract MockSingleUseScript is SingleUseScript {
   event SuccessfulCall();
 
