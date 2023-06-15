42bcbad905b365876a39ef46c45ff6a81e368fa6
dd0sxx <contacttheotelonis@gmail.com>
2023-06-15 15:06:25 -0600

    Update test/mock/MockLlamaAbsoluteStrategyBase.sol
    Co-authored-by: Matt Solomon <matt@mattsolomon.dev>

--- test/mock/MockLlamaAbsoluteStrategyBase.sol
+++ test/mock/MockLlamaAbsoluteStrategyBase.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.19;
 
 import {ActionInfo} from "src/lib/Structs.sol";
-import {ILlamaStrategy} from "src/interfaces/ILlamaStrategy.sol";
 import {LlamaAbsoluteStrategyBase} from "src/strategies/LlamaAbsoluteStrategyBase.sol";
 
 // @dev A mock account implementation (logic) contract that doesn't have `name` or other functions. To be used for
