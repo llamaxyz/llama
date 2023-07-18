b2c753d6f7eba09432dc3ba50a3698770594c30c
dd0sxx <contacttheotelonis@gmail.com>
2023-07-18 12:22:04 +0200

    LlamaRelativeQuantityQuorum

--- src/strategies/LlamaRelativeQuantityQuorum.sol
+++ src/strategies/LlamaRelativeQuantityQuorum.sol
@@ -8,24 +8,24 @@ import {LlamaPolicy} from "src/LlamaPolicy.sol";
 
 import {LlamaRelativeStrategyBase} from "src/strategies/LlamaRelativeStrategyBase.sol";
 
-/// @title Llama Relative Holder Quorum Strategy
+/// @title Llama Relative Quantity Quorum Strategy
 /// @author Llama (devsdosomething@llama.xyz)
 /// @notice This is a Llama strategy which has the following properties:
-///   - Approval/disapproval thresholds are specified as percentages of total supply.
+///   - Approval/disapproval thresholds are specified as percentages of total quantity.
 ///   - Action creators are allowed to cast approvals or disapprovals on their own actions within this strategy.
-///   - The approval and disapproval role holder supplies are saved at action creation time and used to calculate that
+///   - The approval and disapproval role holder quantities are saved at action creation time and used to calculate that
 ///     action's quorum.
 ///   - Role quantity is used to determine the approval and disapproval weight of a policyholder's cast.
-contract LlamaRelativeHolderQuorum is LlamaRelativeStrategyBase {
+contract LlamaRelativeQuantityQuorum is LlamaRelativeStrategyBase {
   /// @inheritdoc ILlamaStrategy
   function validateActionCreation(ActionInfo calldata actionInfo) external override {
     if (msg.sender != address(llamaCore)) revert OnlyLlamaCore();
 
     LlamaPolicy llamaPolicy = policy; // Reduce SLOADs.
-    uint256 approvalPolicySupply = llamaPolicy.getRoleSupplyAsNumberOfHolders(approvalRole);
+    uint256 approvalPolicySupply = llamaPolicy.getRoleSupplyAsQuantitySum(approvalRole);
     if (approvalPolicySupply == 0) revert RoleHasZeroSupply(approvalRole);
 
-    uint256 disapprovalPolicySupply = llamaPolicy.getRoleSupplyAsNumberOfHolders(disapprovalRole);
+    uint256 disapprovalPolicySupply = llamaPolicy.getRoleSupplyAsQuantitySum(disapprovalRole);
     if (disapprovalPolicySupply == 0) revert RoleHasZeroSupply(disapprovalRole);
 
     // Save off the supplies to use for checking quorum.
