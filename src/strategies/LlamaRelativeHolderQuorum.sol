c48da1a79fdff2d4dc5533a682fcdb77cc5f0b69
Austin Green <austinzgreen@gmail.com>
2023-07-19 14:17:02 -0400

    get past supply

--- src/strategies/LlamaRelativeHolderQuorum.sol
+++ src/strategies/LlamaRelativeHolderQuorum.sol
@@ -22,10 +22,11 @@ contract LlamaRelativeHolderQuorum is LlamaRelativeStrategyBase {
     if (msg.sender != address(llamaCore)) revert OnlyLlamaCore();
 
     LlamaPolicy llamaPolicy = policy; // Reduce SLOADs.
-    uint256 approvalPolicySupply = llamaPolicy.getRoleSupplyAsNumberOfHolders(approvalRole);
+    uint256 approvalPolicySupply = llamaPolicy.getPastRoleSupplyAsNumberOfHolders(approvalRole, block.timestamp - 1);
     if (approvalPolicySupply == 0) revert RoleHasZeroSupply(approvalRole);
 
-    uint256 disapprovalPolicySupply = llamaPolicy.getRoleSupplyAsNumberOfHolders(disapprovalRole);
+    uint256 disapprovalPolicySupply =
+      llamaPolicy.getPastRoleSupplyAsNumberOfHolders(disapprovalRole, block.timestamp - 1);
     if (disapprovalPolicySupply == 0) revert RoleHasZeroSupply(disapprovalRole);
 
     // Save off the supplies to use for checking quorum.
