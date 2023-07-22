e7ba5cf0036f7996728d1f1789f0aaaf12022c98
Rajath Alex <rajathalex@gmail.com>
2023-07-22 13:03:44 -0400

    LlamaAbsoluteQuorum rolehaszerosupply tests

--- test/strategies/absolute/LlamaAbsoluteQuorum.t.sol
+++ test/strategies/absolute/LlamaAbsoluteQuorum.t.sol
@@ -126,6 +126,50 @@ contract ValidateActionCreation is LlamaAbsoluteQuorumTest {
 
     mpCore.castDisapproval(uint8(Roles.TestRole1), actionInfo, "");
   }
+
+  function test_RevertIf_ApprovalRoleHasZeroSupply() public {
+    // Getting a role with no supply currently and initializing it.
+    uint8 noSupplyRole = mpPolicy.numRoles() + 1;
+    initializeRolesUpTo(noSupplyRole);
+
+    // Create the strategy with 0 (dis)approval threshold to not trigger `InvalidMinApprovals` error.
+    ILlamaStrategy testStrategy = deployAbsoluteQuorum(
+      noSupplyRole, uint8(Roles.Disapprover), 1 days, 4 days, 1 days, true, 0, 0, new uint8[](0), new uint8[](0)
+    );
+
+    // Give the action creator the ability to use this strategy.
+    bytes32 newPermissionId = keccak256(abi.encode(address(mockProtocol), PAUSE_SELECTOR, testStrategy));
+    vm.prank(address(mpExecutor));
+    mpPolicy.setRolePermission(uint8(Roles.ActionCreator), newPermissionId, true);
+
+    // Create the action.
+    bytes memory data = abi.encodeCall(MockProtocol.pause, (true));
+    vm.prank(actionCreatorAaron);
+    vm.expectRevert(abi.encodeWithSelector(LlamaAbsoluteStrategyBase.RoleHasZeroSupply.selector, noSupplyRole));
+    mpCore.createAction(uint8(Roles.ActionCreator), testStrategy, address(mockProtocol), 0, data, "");
+  }
+
+  function test_RevertIf_DisapprovalRoleHasZeroSupply() public {
+    // Getting a role with no supply currently and initializing it.
+    uint8 noSupplyRole = mpPolicy.numRoles() + 1;
+    initializeRolesUpTo(noSupplyRole);
+
+    // Create the strategy with 0 (dis)approval threshold to not trigger `InvalidMinApprovals` error.
+    ILlamaStrategy testStrategy = deployAbsoluteQuorum(
+      uint8(Roles.Approver), noSupplyRole, 1 days, 4 days, 1 days, true, 0, 0, new uint8[](0), new uint8[](0)
+    );
+
+    // Give the action creator the ability to use this strategy.
+    bytes32 newPermissionId = keccak256(abi.encode(address(mockProtocol), PAUSE_SELECTOR, testStrategy));
+    vm.prank(address(mpExecutor));
+    mpPolicy.setRolePermission(uint8(Roles.ActionCreator), newPermissionId, true);
+
+    // Create the action.
+    bytes memory data = abi.encodeCall(MockProtocol.pause, (true));
+    vm.prank(actionCreatorAaron);
+    vm.expectRevert(abi.encodeWithSelector(LlamaAbsoluteStrategyBase.RoleHasZeroSupply.selector, noSupplyRole));
+    mpCore.createAction(uint8(Roles.ActionCreator), testStrategy, address(mockProtocol), 0, data, "");
+  }
 }
 
 contract IsApprovalEnabled is LlamaAbsoluteQuorumTest {
