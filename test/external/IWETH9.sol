8ee6ba81bf5ea409ce05587aa12b46f488d6d99f
Rajath Alex <rajathalex@gmail.com>
2023-07-28 20:27:11 -0400

    IWETH9 testing interface

---
+++ test/external/IWETH9.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IWETH9 {
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+
+    function approve(address guy, uint256 wad) external returns (bool);
+
+    function transfer(address dst, uint256 wad) external returns (bool);
+
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 wad
+    ) external returns (bool);
+}
