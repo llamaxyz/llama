52ba4a312eb61ae84ef473f0299a81124cbddfc0
dd0sxx <contacttheotelonis@gmail.com>
2023-08-02 21:43:15 -0600

    fmt

--- src/interfaces/ILlamaStrategy.sol
+++ src/interfaces/ILlamaStrategy.sol
@@ -24,7 +24,8 @@ interface ILlamaStrategy {
   /// @notice Initializes a new clone of the strategy.
   /// @param config The strategy configuration, encoded as bytes to support differing constructor arguments in
   /// different strategies.
-  /// @return This return statement must be harcoded to `true` to ensure that initializing an EOA (like the zero address)
+  /// @return This return statement must be harcoded to `true` to ensure that initializing an EOA (like the zero
+  /// address)
   /// will revert.
   function initialize(bytes memory config) external returns (bool);
 
