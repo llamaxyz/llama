5bdf957de25ed337923a7b1c7adc30047d1af5a7
dd0sxx <contacttheotelonis@gmail.com>
2023-08-09 22:29:39 -0700

    weird evm error

--- test/integrations/Llama.integrations.t.sol
+++ test/integrations/Llama.integrations.t.sol
@@ -3,7 +3,17 @@ pragma solidity ^0.8.19;
 
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 
+import {DeployLlamaInstance} from "script/DeployLlamaInstance.s.sol";
+import {DeployUtils} from "script/DeployUtils.sol";
+
 import {LlamaTestSetup} from "test/utils/LlamaTestSetup.sol";
+
+import {ILlamaPolicyMetadata} from "src/interfaces/ILlamaPolicyMetadata.sol";
+import {ILlamaStrategy} from "src/interfaces/ILlamaStrategy.sol";
+import {ILlamaAccount} from "src/interfaces/ILlamaAccount.sol";
+import {LlamaCore} from "src/LlamaCore.sol";
+import {LlamaExecutor} from "src/LlamaExecutor.sol";
+import {LlamaPolicy} from "src/LlamaPolicy.sol";
 
 contract LlamaIntegrationsTest is LlamaTestSetup {
   function setUp() public virtual override {
@@ -26,16 +36,70 @@ contract Setup is LlamaIntegrationsTest {
   }
 }
 
-contract Integration is LlamaIntegrationsTest {
+contract LlamaOrgIntegration is LlamaIntegrationsTest {
+
+  enum LlamaRoles {
+  AllHolders,
+  Founders,
+  Ranchers
+}
+
+  LlamaCore llamaInstanceCore;
+  LlamaExecutor llamaInstanceExecutor;
+  LlamaPolicy llamaInstancePolicy;
+  ILlamaPolicyMetadata llamaInstancePolicyMetadata;
+  ILlamaStrategy llamaInstanceStrategy1;
+  ILlamaStrategy llamaInstanceStrategy2;
+  ILlamaAccount llamaInstanceAccount1;
+  ILlamaAccount llamaInstanceAccount2;
+
+  address shreyas;
+  uint256 shreyasPK;
+  address austin;
+  uint256 austinPK;
+  address llamaDev1;
+  uint256 llamaDev1PK;
+  address llamaDev2;
+  uint256 llamaDev2PK;
+  address llamaDev3;
+  uint256 llamaDevPK;
+
+  
+
+  function setUp() public virtual override {
+    LlamaTestSetup.setUp();
+    (shreyas, shreyasPK) = makeAddrAndKey("Shreyas");
+    (austin, austinPK) = makeAddrAndKey("Austin");
+    (llamaDev1, llamaDev1PK) = makeAddrAndKey("LlamaDev1");
+    (llamaDev2, llamaDev2PK) = makeAddrAndKey("LlamaDev2");
+    (llamaDev3, llamaDevPK) = makeAddrAndKey("LlamaDev3");
+
+    // this line overwrites a global variable from LlamaTestSetup, and takes advantage of the helper functions defined in LlamaTestSetup. Because this is in `setUp()`, it will only overwrite this value for the tests in this contract block.
+    createActionScriptInput = DeployUtils.readScriptInput("mockLlamaIntegration.json");
+
+    DeployLlamaInstance.run(LLAMA_INSTANCE_DEPLOYER, "mockLlamaIntegration.json");
+
+    llamaInstanceCore = core;
+    llamaInstancePolicy = mpCore.policy();
+    llamaInstanceExecutor = mpCore.executor();
+    llamaInstancePolicyMetadata = mpPolicy.llamaPolicyMetadata();
+
+    bytes[] memory instanceStrategyConfigs = DeployUtils.readRelativeStrategies("mockLlamaIntegration.json");
+    // bytes[] memory rootAccounts = DeployUtils.readAccounts("mockLlamaIntegration.json");
+
+    // llamaInstanceAccount1 = lens.computeLlamaAccountAddress(address(accountLogic), rootAccounts[0], address(rootCore));
+    // llamaInstanceAccount2 = lens.computeLlamaAccountAddress(address(accountLogic), rootAccounts[1], address(rootCore));
+
+    // rootStrategy1 =
+    //   lens.computeLlamaStrategyAddress(address(relativeHolderQuorumLogic), instanceStrategyConfigs[1], address(rootCore));
+    // rootStrategy2 =
+    //   lens.computeLlamaStrategyAddress(address(relativeHolderQuorumLogic), instanceStrategyConfigs[2], address(rootCore));
+
+  }
+
   function test_CompleteActionFlow() public {
     // TODO
     // We can use _executeCompleteActionFlow() from LlamaCore.t.sol
-  }
-
-  function testFuzz_NewLlamaInstancesCanBeDeployed() public {
-    // TODO
-    // Test that the root/llama LlamaIntegrations can deploy new client LlamaIntegrations
-    // instances by creating an action to call LlamaFactory.deploy.
   }
 
   function testFuzz_ETHSendFromAccountViaActionApproval(uint256 _ethAmount) public {
