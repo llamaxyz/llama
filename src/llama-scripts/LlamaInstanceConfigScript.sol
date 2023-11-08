da4717b88a1c7a9600a3555dee89b33ce1a736df
Austin Green <austinzgreen@gmail.com>
2023-11-07 19:50:17 -0500

    add base config

--- src/llama-scripts/LlamaInstanceConfigScript.sol
+++ src/llama-scripts/LlamaInstanceConfigScript.sol
@@ -1,53 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {LlamaBaseScript} from "src/llama-scripts/LlamaBaseScript.sol";
+import {LlamaInstanceConfigBase} from "src/llama-scripts/LlamaInstanceConfigBase.sol";
 import {ILlamaStrategy} from "src/interfaces/ILlamaStrategy.sol";
 import {LlamaCore} from "src/LlamaCore.sol";
-import {LlamaPolicy} from "src/LlamaPolicy.sol";
 import {PermissionData} from "src/lib/Structs.sol";
 import {RoleDescription} from "src/lib/UDVTs.sol";
 
-contract LlamaInstanceConfigScript is LlamaBaseScript {
-  uint8 constant CONFIG_ROLE = 1;
-
-  function _postConfigurationCleanup(
-    address configPolicyHolder,
-    LlamaCore core,
-    ILlamaStrategy bootstrapStrategy,
-    RoleDescription description
-  ) internal {
-    LlamaPolicy policy = core.policy();
-    PermissionData memory authorizePermission =
-      PermissionData(address(core), LlamaCore.setScriptAuthorization.selector, bootstrapStrategy);
-    PermissionData memory executePermission =
-      PermissionData(SELF, LlamaInstanceConfigScript.execute.selector, bootstrapStrategy);
-
-    // Rename role #1 description
-    policy.updateRoleDescription(CONFIG_ROLE, description);
-
-    // Unauthorize configuration script
-    core.setScriptAuthorization(SELF, false);
-
-    // Remove configuration policyholder
-    policy.revokePolicy(configPolicyHolder);
-
-    // Unauthorize instant execution strategy
-    core.setStrategyAuthorization(bootstrapStrategy, false);
-
-    // Remove role #1 permissions to authorize and execute scripts
-    policy.setRolePermission(CONFIG_ROLE, authorizePermission, false);
-    policy.setRolePermission(CONFIG_ROLE, executePermission, false);
-  }
-
+contract LlamaInstanceConfigScript is LlamaInstanceConfigBase {
   function execute(address configPolicyHolder, ILlamaStrategy bootstrapStrategy, RoleDescription description)
     external
     onlyDelegateCall
   {
     LlamaCore core = LlamaCore(msg.sender);
+    PermissionData memory executePermission =
+      PermissionData(SELF, LlamaInstanceConfigScript.execute.selector, bootstrapStrategy);
 
     // Insert configuration code here
 
-    _postConfigurationCleanup(configPolicyHolder, core, bootstrapStrategy, description);
+    _postConfigurationCleanup(configPolicyHolder, core, bootstrapStrategy, description, executePermission);
   }
 }
