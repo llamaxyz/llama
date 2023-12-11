083a6ac4dc6a04669bd41b3e6a22db47417c15f9
Austin Green <austinzgreen@gmail.com>
2023-12-11 17:58:37 -0500

    script

--- src/llama-scripts/LlamaAccountTokenDelegationScript.sol
+++ src/llama-scripts/LlamaAccountTokenDelegationScript.sol
@@ -6,9 +6,26 @@ import {LlamaAccount} from "src/accounts/LlamaAccount.sol";
 
 /// @title Llama Account Token Delegation Script
 /// @author Llama (devsdosomething@llama.xyz)
-/// @notice A script that allows users to delegate governance tokens in their Llama accounts.
+/// @notice A script that leverages the `LlamaAccount` arbitrary execute function to allow users to delegate governance
+/// tokens from their Llama accounts.
 contract LlamaAccountTokenDelegationScript is LlamaBaseScript {
-  function delegateAccountTokenToExecutor(LlamaAccount account, address token) external onlyDelegateCall {
-    account.execute(token, false, 0, abi.encodeWithSelector(0x5c19a95c, address(this)));
+  /// @notice Token contract must be called not delegatecalled.
+  bool internal constant WITH_DELEGATECALL = false;
+
+  /// @notice `msg.value` is 0.
+  uint256 internal constant VALUE = 0;
+
+  /// @notice The function selector of the `IVotes` `delegate(address)` function.
+  bytes4 internal constant DELEGATE_SELECTOR = 0x5c19a95c;
+
+  function delegateTokenFromAccount(LlamaAccount account, address token, address delegatee) external onlyDelegateCall {
+    account.execute(token, WITH_DELEGATECALL, VALUE, abi.encodeWithSelector(DELEGATE_SELECTOR, delegatee));
+  }
+
+  function delegateTokensFromAccount(LlamaAccount account, address[] tokens, address delegatee)
+    external
+    onlyDelegateCall
+  {
+    account.execute(token, WITH_DELEGATECALL, VALUE, abi.encodeWithSelector(DELEGATE_SELECTOR, delegatee));
   }
 }
